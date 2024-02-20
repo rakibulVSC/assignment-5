617ca8274818f625d9e64ba770281d63dfd358bf
rakibulVSC <mrakibulhasan755@gmail.com>
2024-02-20 10:41:00 +0600

    add resolved text

--- styles/ticket.js
+++ styles/ticket.js
@@ -1,47 +1,42 @@
 // add div 
-function createNewDiv(){
-const newDiv=document.createElement("div");
-newDiv.classList.add("flex", "justify-between");
-const paragraph=document.createElement("p");
-paragraph.innerText="value";
-newDiv.appendChild(paragraph);
-const para=document.createElement("p");
-para.textContent="Economy";
-newDiv.appendChild(para);
-const parah=document.createElement("p");
-parah.textContent="550";
-newDiv.appendChild(parah);
-document.getElementById('class').appendChild(newDiv);
+// function createNewDiv(){
+// const newDiv=document.createElement("div");
+// newDiv.classList.add("flex", "justify-between");
+// const paragraph=document.createElement("p");
+// paragraph =cards;
+// // paragraph.innerText="";
+// newDiv.appendChild(paragraph);
+// const para=document.createElement("p");
+// para.textContent="Economy";
+// newDiv.appendChild(para);
+// const parah=document.createElement("p");
+// parah.textContent="550";
+// newDiv.appendChild(parah);
+// document.getElementById('class').appendChild(newDiv);
 
 
-}
-createNewDiv();
-
-
-
-
+// }
+// createNewDiv();
 
 
 const cards=document.querySelectorAll("#card");
 // console.log(cards);
 
-
-
 for(const card of cards){
     // console.log(ticket)
-  card.addEventListener('click',function(){
+  card.addEventListener('click',function(event){
     // console.log('clicked');
     // get the name,class,price 
     const title=card.querySelector("h4");
     let value=title.innerText;
-    console.log(value);
-
+    // console.log(value);
+  
   });
-//  let ChangeValue=card.querySelectorAll(".changeValue") ;
-//  ChangeValue.addEventListener('click',function(){
-//   let para=card.querySelectorAll("p");
-//   paragraph.innerText="New Value";
-//  })
+
 
 }
+
+
+
+
 
